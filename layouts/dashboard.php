5f1013cc1c99f53f6f989e92f4e0bd6ef18160eb
riskkif <riskisam78@gmail.com>
2023-12-12 16:22:36 +0700

    ci: link bootstrap

--- layouts/dashboard.php
+++ layouts/dashboard.php
@@ -11,7 +11,7 @@
 	<link rel="shortcut icon" href="<?php echo AST; ?>/img/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="<?php echo AST; ?>/datatables/datatables.min.css">
 	<link rel="stylesheet" href="<?php echo AST; ?>/css/style.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	
 
 	<script src="<?php echo AST; ?>/datatables/datatables.min.js"></script>
 
@@ -80,7 +80,7 @@
 			paging: true
 		});
 	</script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	
 
 </body>
 
